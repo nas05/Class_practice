12cf19eb8280620e9a02690010082c37f4288ea9
nas05 <traore_nasser@yahoo.fr>
2022-09-13 19:59:29 -0400

    Add files via upload

---
+++ example.py
@@ -0,0 +1,34 @@
+
+
+number1 = int(input('enter the first number: '))
+number2 = int(input('enter the second number: '))
+Sum = number1 + number2
+minus = number1 - number2
+multiply = number1 * number2
+power = number1**number2
+divide = number1/number2
+print(number1,' + ',number2,' = ',Sum)
+print(number1,' - ',number2,' = ',minus)
+print(number1,'* ',number2,' = ',multiply)
+print(number1,' ** ', number2,' is:' ,power)
+print(number1,' / ',number2,' = ',divide)
+
+
+
+
+#my_integer_variable=1000
+#my_float_variable=3.5
+#my_complex_variable=1+2j
+
+#print(type(my_integer_variable))
+#print(type(my_float_variable))
+#print(type(my_complex_variable))
+#string_one ='name '
+#string_two= 'is '
+#string_tree= 'Nas'
+#print(string_one,string_two,string_tree)
+#print(string_one + string_two + string_tree)
+
+#print(strings)
+
+#print(7%2)
